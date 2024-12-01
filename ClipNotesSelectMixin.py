e65ab6c3bcb333d71dada1613e0efeaae83491cc
chiaki <64439080+chiakibeats@users.noreply.github.com>
2024-12-01 15:27:01 +0900

    Add deselect notes after events button pressed

--- ClipNotesSelectMixin.py
+++ ClipNotesSelectMixin.py
@@ -11,6 +11,7 @@ class ClipNotesSelectMixin():
     note_select_button = ButtonControl(color = None)
 
     _sequencer_clip = None
+    _trigger_deselect = True
 
     @depends(sequencer_clip = None)
     def __init__(self, sequencer_clip = None, *a, **k):
@@ -27,12 +28,16 @@ class ClipNotesSelectMixin():
                 pitch_span = 1)
             
             note_ids = [note.note_id for note in notes]
-            clip.deselect_all_notes()
+            if self._trigger_deselect:
+                clip.deselect_all_notes()
+                self._trigger_deselect = False
             clip.select_notes_by_id(note_ids)
 
     @note_select_button.value
     def _on_note_select_button_changed(self, value, button):
         self._set_control_pads_from_script(button.is_pressed)
+        if not button.is_pressed:
+            self._trigger_deselect = True
 
     @note_select_button.double_clicked
     def _on_note_select_double_clicked(self, button):
