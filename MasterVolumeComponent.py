e8b57205be1ded1c8d53367a5357dd8aa1b950d2
chiaki <nowhere>
2025-01-02 01:42:25 +0900

    Fix control target to the master track

--- MasterVolumeComponent.py
+++ MasterVolumeComponent.py
@@ -2,33 +2,32 @@ from ableton.v3.control_surface.component import Component
 from ableton.v3.control_surface.display import Renderable
 from ableton.v3.control_surface.controls import StepEncoderControl, MappedControl, ButtonControl
 from .Logger import logger
-from ableton.v3.base import clamp, sign, listens, depends
+from ableton.v3.base import clamp, sign, listens, depends, listenable_property
 
 
-class MasterVolumeComponent(Component):
-    coarse_volume = StepEncoderControl(num_steps = 64)
-    fine_volume = StepEncoderControl(num_steps = 64)
+class MasterVolumeComponent(Component, Renderable):
+    master_volume = StepEncoderControl(num_steps = 64)
     reset_button = ButtonControl(color = None)
 
-    _target_track = None
-
-    @depends(target_track = None)
     def __init__(self, name = "Master_Volume", target_track = None, *a, **k):
         super().__init__(name, *a, **k)
-        self._target_track = target_track
+        self._master_volume = self.song.master_track.mixer_device.volume
+        self._on_master_volume_changed.subject = self._master_volume
 
-    # roughly 1dB / 0.1dB step gain control
-    @coarse_volume.value
+    @listenable_property
+    def gain_string(self):
+        return self._master_volume.str_for_value(self._master_volume.value)
+
+    # Roughly 0.1dB step gain control
+    # +6 to -18dB range has perfect linearity, lower range has different (exponential) scale.
+    @master_volume.value
     def _on_coarse_volume_changed(self, value, encoder):
-        volume_parameter = self._target_track.target_track.mixer_device.volume
-        volume_parameter.value = clamp(volume_parameter.value + sign(value) / 40, 0.0, 1.0)
-        
-    @fine_volume.value
-    def _on_fine_volume_changed(self, value, encoder):
-        volume_parameter = self._target_track.target_track.mixer_device.volume
-        volume_parameter.value = clamp(volume_parameter.value + sign(value) / 400, 0.0, 1.0)
-    
+        self._master_volume.value = clamp(self._master_volume.value + sign(value) / 400, 0.0, 1.0)
+            
     @reset_button.pressed
     def _on_reset_button_pressed(self, button):
-        volume_parameter = self._target_track.target_track.mixer_device.volume
-        volume_parameter.value = volume_parameter.default_value
+        self._master_volume.value = self._master_volume.default_value
+
+    @listens("value")
+    def _on_master_volume_changed(self):
+        self.notify_gain_string()
