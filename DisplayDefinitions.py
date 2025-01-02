47e36f598f259165fc521f3c3441e0a0f435a1d0
chiaki <nowhere>
2025-01-02 20:00:52 +0900

    Change display mechanism drastically (now Content class has actual display contents only) Enable notification feature Rewrite clip editor and browser view Add mixer view

--- DisplayDefinitions.py
+++ DisplayDefinitions.py
@@ -1,14 +1,16 @@
 from dataclasses import dataclass
 from ableton.v3.control_surface.display import DefaultNotifications, DisplaySpecification
 from ableton.v3.control_surface.display.view import View, CompoundView, NotificationView
-from ableton.v3.live import liveobj_name, liveobj_valid
 from ableton.v3.control_surface.display.text import adjust_string
+from ableton.v3.control_surface.display.notifications.type_decl import Fn, Notification
+from ableton.v3.live import liveobj_name, liveobj_valid
 
 from .ClipEditorComponent import LaunchModeList, ClipLaunchQuantizationList, WarpModeList
 from .Logger import logger
 
 LCD_LINES = 4
 LCD_LINE_LENGTH = 28
+NO_ITEM = "---"
 
 def make_mcu_display_header(line):
     return (0xF0, 0x00, 0x00, 0x66, 0x17, 0x12, 28 * min(line, LCD_LINES - 1))
@@ -16,13 +18,16 @@ def make_mcu_display_header(line):
 def make_display_sysex_message(line, message):
     return make_mcu_display_header(line) + message + (0xF7,)
 
-class Content:
+class OldContent:
     selected_mode = None
     selected_track = None
     selected_device = None
     selected_parameter_bank = None
     current_tracks_in_ring = []
     selected_clip = None
+    clip_start_marker = ""
+    clip_loop_length = ""
+    clip_loop_offset = ""
     selected_folder = None
     selected_browser_item = None
     selected_pitches = []
@@ -35,40 +40,188 @@ class Content:
     display_mode = None
     target_track_locked = False
 
+class Content:
+    lines = [""] * 4
+
+SESSION_RING = "default"
+MASTER_VOLUME = "volume"
+GROOVE_AMOUNT = "swing"
+ARRANGE_POSITION = "position"
+SONG_TEMPO = "tempo"
+CLIP_SCALE = "scale"
+ENCODER_MODES = (SESSION_RING, MASTER_VOLUME, GROOVE_AMOUNT, ARRANGE_POSITION, SONG_TEMPO, CLIP_SCALE)
+
+TRACK_MIXER = "default"
+DEVICE_CONTROL = "device"
+CLIP_CONTROL = "clip"
+BROWSER = "browser"
+
+
 class Notifications(DefaultNotifications):
 
     class Device(DefaultNotifications.Device):
+        lock = DefaultNotifications.DefaultText()
+        select = DefaultNotifications.DefaultText()
         bank = DefaultNotifications.DefaultText()
+
+    class Track(DefaultNotifications.Track):
+        lock = DefaultNotifications.DefaultText()
+
+    class Recording(DefaultNotifications.Recording):
+        fixed_length = "Fixed Length Rec:{}".format
+        fixed_length: "Notification[Fn[str]]"
+
+    class NoteRepeat:
+        repeat_rate = "Note Repeat Rate:{}".format
+        repeat_rate: "Notification[Fn[str]]"
+    
+    class DrumGroup(DefaultNotifications.DrumGroup):
+        class Pad(DefaultNotifications.DrumGroup.Pad):
+            select = DefaultNotifications.DefaultText()
+
 
 def try_get_attr(obj, attr, default = None):
     if obj != None:
         return getattr(obj, attr)
     else:
         return default
+    
+def adjust_gain_string(gain_string):
+    if len(gain_string) > 0:
+        gain_string = gain_string[:-3]
+        if gain_string[0] != "-":
+            gain_string = " " + gain_string
+        if len(gain_string) > 6:
+            gain_string = gain_string[:6]
+
+    return gain_string
 
 def create_root_view():
     logger.info("Init display")
+
+    def to_pan_or_send_value(knob):
+        if str.startswith(knob.parameter_name, "Pan"):
+            return knob.parameter_value
+        else:
+            return adjust_gain_string(knob.parameter_value)
+
+    def mixer_view(state, content):
+        control_name = state.mixer.control_name
+        content.lines[0] = f"Parameter:{control_name}"
+        content.lines[2] = "{:<6}|{:<6}|{:<6}|{:<6}".format(*[to_pan_or_send_value(knob) for knob in state.elements.knobs[:4]])
+
+        content.lines[1] = f"{'Locked' if state.target_track.is_locked_to_track else 'Target'}:{state.target_track.target_track.name}"
+        content.lines[3] = "{:<6}|{:<6}|{:<6}|{:<6}".format(*[adjust_gain_string(knob.parameter_value) for knob in state.elements.knobs[4:]])
+
+    def device_view(state, content):
+        content.lines[0] = ""
+        content.lines[1] = ""
+        
+    def clip_view(state, content):
+        clip = state.target_track.target_clip
+
+        if liveobj_valid(clip):
+            name = adjust_string(clip.name, 13)
+            if clip.looping:
+                content.lines[0] = f"{name:<13}|Off:{state.clip_editor.start_marker:<10}"
+                content.lines[2] = f"S:{state.clip_editor.loop_offset:<11}|Len:{state.clip_editor.loop_length}"
+            else:
+                content.lines[0] = f"{name:<13}|Non-looped"
+                content.lines[2] = f"S:{state.clip_editor.loop_start:<11}|E:{state.clip_editor.loop_end:<11}"
+
+            launch_mode = adjust_string(LaunchModeList.to_string(clip.launch_mode), 6)
+            quantize = ClipLaunchQuantizationList.to_string(clip.launch_quantization)
+            #quantization = ClipLaunchQuantizationList.to_string(clip.launch_quantization)
+            if clip.is_audio_clip:
+                content.lines[1] = f"{launch_mode:<6}|{quantize:>6}|Legato|Warp"
+                warp = WarpModeList.to_string(clip.warp_mode) if clip.warping else "No Warp"
+                pitch = f"{clip.pitch_coarse + clip.pitch_fine * 0.01:+.2f}st"
+                gain = adjust_gain_string(clip.gain_display_string)
+                # 7 + 9 + 9 = 25
+                content.lines[3] = f"{gain:<6}|{pitch:>8}|{warp:<8}"
+            else:
+                content.lines[1] = f"{launch_mode:<6}|{quantize:>6}|Legato|"
+                content.lines[3] = "Nudge |Steps |Fine  |Vel"
+        else:
+            content.lines[0] = "No Clip Selected"
+            content.lines[1] = ""
+            content.lines[2] = ""
+            content.lines[3] = ""
+
+    def browser_view(state, content):
+        content.lines[0] = ""
+        folder_name = state.browser.parent_folder_name or NO_ITEM
+        item_name = state.browser.selected_item_name or NO_ITEM
+
+        content.lines[0] = f"In:{folder_name}"
+        content.lines[1] = ""
+        content.lines[2] = f">{item_name[:LCD_LINE_LENGTH - 1]}"
+        content.lines[3] = f"{item_name[LCD_LINE_LENGTH - 1:]}"
+
     @View
     def main_view(state):
+        logger.info(f"state = {dir(state)}")
         content = Content()
+        display_mode = state.buttons_and_knobs_modes.selected_mode
+        if display_mode == TRACK_MIXER:
+            mixer_view(state, content)
+        elif display_mode == DEVICE_CONTROL:
+            device_view(state, content)
+        elif display_mode == CLIP_CONTROL:
+            clip_view(state, content)
+        elif display_mode == BROWSER:
+            browser_view(state, content)
 
-        content.selected_track = state.target_track.target_track
-        content.selected_clip = state.target_track.target_clip
-        content.target_track_locked = state.target_track.is_locked_to_track
-        content.selected_device = state.device.device
-        content.selected_parameter_bank = state.device.bank_name
-        content.selected_folder = try_get_attr(state.browser.parent_folder, "name", "---")
-        content.selected_browser_item = state.browser.selected_item
-        content.display_mode = state.buttons_and_knobs_modes.selected_mode
-
+        if state.elements.setting.is_pressed:
+            content.lines[0] = "CustomMaschineMK3 by chiaki"
+            content.lines[2] = "Version 0.99"
+        
+        return content
+    
+    def render_knob_control_view(state):
+        #content = Content()
+        content = main_view(state)
+    
+        if state.elements.encodercap.is_pressed:
+            encoder_mode = state.encoder_modes.selected_mode
+            if encoder_mode == MASTER_VOLUME:
+                content.lines[0] = "Master Volume:"
+                content.lines[2] = f"{state.master_volume.gain_string}"
+            elif encoder_mode == GROOVE_AMOUNT:
+                content.lines[0] = "Groove Amount:"
+                content.lines[2] = f"{state.groove_pool.amount_string}"
+            elif encoder_mode == ARRANGE_POSITION:
+                content.lines[0] = "Playing Cursor:"
+                content.lines[2] = "{}"
+            elif encoder_mode == SONG_TEMPO:
+                content.lines[0] = "Song Tempo:"
+                content.lines[2] = "{}BPM"
+            elif encoder_mode == CLIP_SCALE:
+                content.lines[0] = f"Scale({'On' if state.scale_system.scale_mode else 'Off'}):{state.scale_system.scale_name}"
+                content.lines[2] = f"Root Note:{state.scale_system.root_note}"
         return content
 
-    def notification_content(state, event):
-        logger.info(f"View notification event {event}")
-        main_content = main_view(state)
-        return main_content
+    def should_render_knob_control_view(state):
+        do_render = False
+        #logger.info(f"{state.elements.knob_touch_buttons}")
+        for button in state.elements.knob_touch_buttons:
+            if button.is_pressed:
+                do_render = True
+        
+        if state.elements.encodercap.is_pressed:
+            do_render = True
 
-    return CompoundView(NotificationView(notification_content), main_view)
+        return do_render
+
+    knob_control_view = View(render_knob_control_view, should_render_knob_control_view)
+
+    def notification_content(state, event):
+        logger.info(f"notification: {event}")
+        content = main_view(state)
+        content.lines[0] = event
+        return content
+
+    return CompoundView(NotificationView(notification_content, duration = 1.5), knob_control_view, main_view)
 
 def protocol(elements):
 
@@ -88,81 +241,13 @@ def protocol(elements):
         elements.display_line_0.display_message(f"Device:{device_name}")
         elements.display_line_2.display_message(f"Bank:{bank_name}")
 
-    def display_clip_info(content: Content):
-        clip =  content.selected_clip
-        name = " "
-        loop_offset = " "
-        loop_length = " "
-        start_marker = " "
-        mute = " "
-        loop = "  "
-        launch_mode = " "
-        quantize = " "
-        legato = "   "
-        warp = " "
-        pitch = " "
-        gain = " "
-        clip_type = 0
-
-        if liveobj_valid(clip):
-            name = adjust_string(clip.name, 13)
-            loop_offset = str(clip.position)
-            loop_length = str(clip.loop_end - clip.loop_start)
-            start_marker = str(clip.start_marker)
-            mute = "M" if clip.muted else " "
-            loop = "LP" if clip.looping else "  "
-            launch_mode = LaunchModeList.to_short_string(clip.launch_mode)
-            quantize = ClipLaunchQuantizationList.to_string(clip.launch_quantization)
-            legato = "LEG" if clip.legato else "   "
-            #quantization = ClipLaunchQuantizationList.to_string(clip.launch_quantization)
-            if clip.is_audio_clip:
-                warp = WarpModeList.to_string(clip.warp_mode) if clip.warping else "No Warp"
-                pitch = f"{clip.pitch_coarse:-}"
-                gain = clip.gain_display_string
-                clip_type = 1
-            else:
-                clip_type = 2
-
-        elements.display_line_0.display_message(f"{name:<13}|Off:{start_marker:<10}")
-        elements.display_line_2.display_message(f"S:{loop_offset:<11}|Len:{loop_length:<10}")
-        # 2 + 3 + 5 + 7 + 3 = 20
-        elements.display_line_1.display_message(f"{mute}|{loop}|{launch_mode:<4}|{quantize:<6}|{legato}|")
-        if clip_type == 1:
-            # 9 + 6 + 9 = 24
-            message = f"{warp:<8}|{pitch:>3}st|{gain:>8}"
-        elif clip_type == 2:
-            message = "Nudge |Steps |Fine  |Vel"
-        else:
-            message = ""
-        
-        elements.display_line_3.display_message(message)
-
-    def display_browser(content: Content):
-        item = content.selected_browser_item
-        name = item.name if item != None else "---"
-        elements.display_line_0.display_message(f"In:{content.selected_folder}")
-        elements.display_line_1.display_message("")
-        elements.display_line_2.display_message(f">{name[:LCD_LINE_LENGTH - 1]}")
-        elements.display_line_3.display_message(f"{name[LCD_LINE_LENGTH - 1:]}")
-
     def display(content: Content):
+        #logger.info("display()")
         if content:
-            # elements.display_line_0.display_message("CustomMaschineMK3 by chiaki")
-            # elements.display_line_2.display_message("Version 0.8")
-            if content.display_mode == "default":
-                display_mixer_info(content)
-            elif content.display_mode == "device":
-                display_device_info(content)
-            elif content.display_mode == "clip":
-                display_clip_info(content)
-            elif content.display_mode == "browser":
-                #logger.info(f"folder = {content.selected_folder}, type = {type(content.selected_folder)}")
-                # elements.display_line_0.display_message(f"In:{content.selected_folder}")
-                # elements.display_line_2.display_message(f">{content.selected_browser_item.name}")
-                display_browser(content)
-            else:
-                elements.display_line_0.display_message(f"Track:{content.selected_track}")
-                elements.display_line_2.display_message(f"Clip:{content.selected_clip}")
+            elements.display_line_0.display_message(content.lines[0])
+            elements.display_line_1.display_message(content.lines[1])
+            elements.display_line_2.display_message(content.lines[2])
+            elements.display_line_3.display_message(content.lines[3])
 
     return display
 
